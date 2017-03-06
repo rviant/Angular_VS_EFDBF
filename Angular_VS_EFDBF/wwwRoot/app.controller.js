253642c72ad16335006292ebf36ed071e543b76d
Rodolfo Viant <rodolfo.viant@gmail.com>
2017-03-06 08:23:19 -0500

    Added Gulp and created the home directory with the main app, controller and route

---
+++ Angular_VS_EFDBF/wwwRoot/app.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AppCtrl', AppCtrl)
+
+    AppCtrl.$Inject = [];
+
+    function AppCtrl() {
+        var vm = this;
+
+        console.log("Started the App. Loaded App");
+    }
+})()
---
+++ Angular_VS_EFDBF/wwwRoot/routes/app.route.js
@@ -0,0 +1,16 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.config(function ($stateProvider, $urlRouteProvider) {
+			$urlRouteProvider.otherwise('/');
+			$stateProvider
+					.state('home', {
+						url: '/',
+						templateUrl: '../home/home.html',
+						controller: 'AppCtrl',
+						controllerAs: 'vm'
+					})
+		});
+})()
